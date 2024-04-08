8e14d6267dddd02884337c49ba91f432d81de4a6
wooweh <wooweh@proton.me>
2024-04-08 14:49:40 +0200

    Welcome page.

---
+++ scripts.js
@@ -0,0 +1,35 @@
+'use strict';
+// https://github.com/Colo-Codes/mini-projects/blob/main/todo-app/js/index.js
+
+// Constants, helpers
+
+
+
+// Application state
+
+
+
+// State accessor functions (getters and setters)
+
+
+
+// DOM node references
+
+
+
+// DOM update functions
+
+
+
+// Event handlers
+
+
+
+// Event handler bindings
+
+
+
+// Initial setup
+
+
+
