6d359dfd44b6d1509395bdfa3da533f2636c3f15
wooweh <wooweh@proton.me>
2024-04-11 11:12:09 +0200

    Welcome toggle and storage getters/setters.

--- scripts.js
+++ scripts.js
@@ -5,17 +5,43 @@
 
 const COLORS = ["red", "green", "blue", "yellow", "purple", "orange"];
 
+const hideWelcomeJSON = localStorage.getItem("contact_training_hideWelcome");
+const historyJSON = localStorage.getItem("contact_training_history");
+
+const hideWelcome = JSON.parse(hideWelcomeJSON);
+const history = JSON.parse(historyJSON);
+
 // Application state
 const state = {
   time: 30,
   isPaused: false,
   interval: 0,
+  hideWelcome: false,
+  history: [],
 };
 
 // State accessor functions (getters and setters)
 
-function set(key, val) {
+function setState(key, val) {
   state[key] = val;
+}
+
+function getState(key) {
+  return state[key];
+}
+
+function setStorage(key, val) {
+  const valJSON = JSON.stringify(val);
+  localStorage.setItem(`contact_training_${key}`, valJSON);
+}
+
+function getStorage(key) {
+  const valJSON = localStorage.getItem(`contact_training_${key}`);
+  if (valJSON === null) {
+    return null;
+  }
+  const val = JSON.parse(valJSON);
+  return val;
 }
 
 // DOM node references
@@ -25,6 +51,7 @@ const training = document.getElementById("training");
 const time = document.getElementById("time");
 const pauseOptions = document.getElementById("pause-options");
 
+const welcomeToggle = document.getElementById("welcome-toggle");
 const trainButton = document.getElementById("train-button");
 const instructionsButton = document.getElementById("show-instructions");
 const startButton = document.getElementById("start-practice");
@@ -33,9 +60,14 @@ const continueButton = document.getElementById("continue-practice");
 const discardButton = document.getElementById("discard-practice");
 
 const practiceBoard = document.getElementById("practice-board");
+const welcomeToggleCheckbox = document.getElementById(
+  "welcome-toggle-checkbox"
+);
+const welcomeToggleLabel = document.getElementById("welcome-toggle-label");
 
 // DOM update functions
 
+welcomeToggle.addEventListener("click", (e) => toggleWelcome(e));
 trainButton.addEventListener("click", startTraining);
 instructionsButton.addEventListener("click", showInstructions);
 startButton.addEventListener("click", startTimer);
@@ -44,6 +76,25 @@ continueButton.addEventListener("click", continueTraining);
 discardButton.addEventListener("click", discardTraining);
 
 // Event handlers
+let isProcessing = false;
+function toggleWelcome(e) {
+  if (isProcessing) {
+    const newState = !getState("hideWelcome");
+    setState("hideWelcome", newState);
+    setStorage("hideWelcome", newState);
+    colorToggleLabel(newState);
+  }
+  isProcessing = true;
+  setTimeout(() => (isProcessing = false), 50);
+}
+
+function colorToggleLabel(shouldColor) {
+  if (shouldColor) {
+    welcomeToggleLabel.style.color = "#2196f3";
+  } else {
+    welcomeToggleLabel.style.color = "black";
+  }
+}
 
 function startTraining() {
   hide(welcome);
@@ -61,23 +112,24 @@ function startTimer() {
   show(pauseButton);
 
   const interval = setInterval(() => {
-    const remainingTime = state.time;
+    const remainingTime = getState("time");
     if (remainingTime > 0) {
-      set("time", remainingTime - 1);
+      setState("time", remainingTime - 1);
       setTime(remainingTime - 1);
     } else {
       clearInterval(interval);
-      set("time", 30);
+      setState("time", 30);
       setTime(30);
     }
   }, 1000);
 
-  set("interval", interval);
+  setState("interval", interval);
 }
 
 function pauseTimer() {
   clearDot();
-  clearInterval(state.interval);
+  const interval = getState("interval");
+  clearInterval(interval);
   hide(pauseButton);
   show(pauseOptions);
 }
@@ -92,7 +144,7 @@ function discardTraining() {
   show(startButton);
   hide(pauseButton);
   hide(pauseOptions);
-  set("time", 30);
+  setState("time", 30);
   setTime(30);
 }
 
@@ -119,7 +171,6 @@ function getRandomInt(max) {
 function drawDot() {
   const x = getRandomXY().x;
   const y = getRandomXY().y;
-  console.log(practiceBoard.width, practiceBoard.height, x, y);
   const dot = document.createElement("div");
   dot.classList.add("dot");
   dot.style.position = "absolute";
@@ -134,7 +185,6 @@ function getRandomColor() {
 }
 
 function getRandomXY() {
-
   return {
     x: getRandomInt(practiceBoard.offsetWidth),
     y: getRandomInt(practiceBoard.offsetHeight),
@@ -154,3 +204,16 @@ function getBoardXY(event) {
 }
 
 // Initial setup
+if (hideWelcome !== null) {
+  setState("hideWelcome", hideWelcome);
+  colorToggleLabel(hideWelcome);
+  welcomeToggleCheckbox.checked = hideWelcome;
+  if (hideWelcome) {
+    hide(welcome);
+    show(training);
+  }
+}
+
+if (!!history && !!history.length) {
+  setState("history", history);
+}
